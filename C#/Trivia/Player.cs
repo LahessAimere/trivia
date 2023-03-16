3f7c2ab1fa89efe169ff0110c2132f7bb0870084
Lahess <80914959+LahessAimere@users.noreply.github.com>
2023-03-16 19:23:55 +0100

    Add Player name

---
+++ C#/Trivia/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Player
+{
+	string name;
+
+	public Player (String name)
+	{
+		this.name = name;
+	}
+
+	public string Name
+    {
+		get { return name; }
+        set
+        {
+			if(value != name)
+            {
+				name = value;
+            }
+        }
+    }
+}
